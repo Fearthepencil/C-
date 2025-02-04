5abead99362701c808a6708349ec2d4d0926c83a
Pavle Stefanovic <pavles.dev@gmail.com>
2025-02-04 22:51:34 +0100

    Initial Commit

---
+++ C ucenje/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  C ucenje
+//
+//  Created by Pavle Stefanovic on 4.2.25..
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
